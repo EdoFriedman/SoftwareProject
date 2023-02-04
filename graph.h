164a0149a5f346b4249b304be17b04c100b7d7dc
ExpertYoyo666 <mefuga.yt@gmail.com>
2023-02-04 17:27:34 +0200

    "Added Graph Algorithms"

---
+++ graph.h
@@ -0,0 +1,20 @@
+double sqr_euclidean_dist(double* x1,  double* x2, size_t n_dim);
+/**
+ * Computes the squared euclidean distance of two n_dim dimensional
+ * vectors x1, x2.
+ */
+
+Matrix get_w_adj_mat(double** datapoints, size_t n_datapoints, size_t n_dim);
+/**
+ * Computes the Weighted Adjacency Matrix.
+ */
+
+Matrix get_ddg_matrix(Matrix w_adj_matrix);
+/**
+ * Computes the Diagonal Degree Matrix.
+ */
+
+Matrix get_laplacian_matrix(double **datapoints, size_t n_datapoints, size_t n_dim);
+/**
+ * Computes the Graph Laplacian Matrix.
+ */
