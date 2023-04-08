44adc6a1f013d071a1ee9aeeb8b97f0984cf3c55
Edo Friedman <edofriedman@mail.tau.ac.il>
2023-04-08 16:47:45 +0300

    Add code for parsing C-API function arguments

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -1,23 +1,109 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include "matrix.h"
+
+/**
+ * Gets a Python list containing datapoints
+ * Returns a C double** containing the datapoints and sets *datapoint_count and *dim accordingly.
+ */
+double** get_datapoints(PyObject* py_list, size_t* datapoint_count, size_t* dim) {
+    PyObject* py_datapoint;
+    double** datapoints;
+    size_t i;
+    size_t j;
+    *dim = 0;
+    *datapoint_count = PyObject_Length(py_list);
+    datapoints = malloc(*datapoint_count * sizeof(double*));
+    for(i = 0; i < *datapoint_count; i++) {
+        py_datapoint = PyObject_GetItem(py_list, i);
+        if(!*dim) *dim = PyObject_Length(py_datapoint);
+        datapoints[i] = malloc(*dim * sizeof(double));
+        for(j = 0; j < *dim; j++) {
+            datapoints[i][j] = PyFloat_AsDouble(PyObject_GetItem(py_datapoint, j));
+        }
+    }
+    return datapoints;
+}
+
+void free_datapoints(double** datapoints, size_t datapoint_count) {
+    size_t i;
+    for(i = 0; i < datapoint_count; i++) {
+        free(datapoints[i]);
+    }
+    free(datapoints);
+}
 
 static PyObject* spk_py(PyObject* self, PyObject* args) {
+    PyObject* py_initial_centroids;
+    PyObject* py_datapoints;
+    double** initial_centroids;
+    size_t cluster_count;
+    size_t dim;
+    double** datapoints;
+    size_t datapoint_count;
+    if(!PyArg_ParseTuple(args, "OO", &py_initial_centroids, &py_datapoints)) {
+        return NULL;
+    }
+    initial_centroids = get_datapoints(py_initial_centroids, &cluster_count, &dim);
+    datapoints = get_datapoints(py_datapoints, &datapoint_count, &dim);
+
+
+    free_datapoints(initial_centroids, cluster_count);
+    free_datapoints(datapoints, datapoint_count);
     return NULL;
 }
 
 static PyObject* wam_py(PyObject* self, PyObject* args) {
+    PyObject* py_datapoints;
+    double** datapoints;
+    size_t datapoint_count;
+    size_t dim;
+    if(!PyArg_ParseTuple(args, "O", &py_datapoints)) {
+        return NULL;
+    }
+    datapoints = get_datapoints(py_datapoints, &datapoint_count, &dim);
+
+    free_datapoints(datapoints, datapoint_count);
     return NULL;
 }
 
 static PyObject* ddg_py(PyObject* self, PyObject* args) {
+    PyObject* py_datapoints;
+    double** datapoints;
+    size_t datapoint_count;
+    size_t dim;
+    if(!PyArg_ParseTuple(args, "O", &py_datapoints)) {
+        return NULL;
+    }
+    datapoints = get_datapoints(py_datapoints, &datapoint_count, &dim);
+
+    free_datapoints(datapoints, datapoint_count);
     return NULL;
 }
 
 static PyObject* gl_py(PyObject* self, PyObject* args) {
+    PyObject* py_datapoints;
+    double** datapoints;
+    size_t datapoint_count;
+    size_t dim;
+    if(!PyArg_ParseTuple(args, "O", &py_datapoints)) {
+        return NULL;
+    }
+    datapoints = get_datapoints(py_datapoints, &datapoint_count, &dim);
+
+    free_datapoints(datapoints, datapoint_count);
     return NULL;
 }
 
 static PyObject* jacobi_py(PyObject* self, PyObject* args) {
+    PyObject* py_matrix;
+    Matrix matrix;
+    if(!PyArg_ParseTuple(args, "O", &py_matrix)) {
+        return NULL;
+    }
+    matrix.data = get_datapoints(py_matrix, &matrix.shape[0], &matrix.shape[1]);
+
+    mat_free(matrix);
     return NULL;
 }
 
